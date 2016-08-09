d660af032c1a3a68631587e748ddfcf3420ec565
adriaanmarain <adriaanmarain300@gmail.com>
2016-08-09 12:29:50 +0200

    opdracht comments

--- phpstorm/HelloWorld.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: adriaanmarain
- * Date: 09/08/16
- * Time: 11:38
- */
---
+++ phpstorm/opdrachten/comments/comments.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Adriaan Marain
+adriaanmarain300@gmail.com
+*/
+
+$voornaam = "Adriaan";
+$achternaam = "Marain";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/global.css">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/directory.css">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/facade.css">
+</head>
+
+<body class="web-backend-inleiding">
+
+	<section class="body">
+
+		<h1>Comments</h1>
+
+		<p><?= $voornaam, " ", $achternaam ?></p>
+
+</section>
+
+</body>
+</html>
